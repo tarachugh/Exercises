d833b52cb54c11a6fe6cb7ae8fe7b8c159fb64a6
Tara Chugh <Tara@Taras-MacBook.local>
2020-06-30 22:47:17 -0400

    work on exercises

--- introduction_to_python/recursive_functions/3_merge_sort/solutions/solution.py
+++ introduction_to_python/recursive_functions/3_merge_sort/solutions/solution.py
@@ -1,1 +1,37 @@
 # Write your solution here
+def custom_sort(num_list):
+    length=len(num_list)
+    if length<2:
+        return(num_list)
+    L=num_list[0:(length//2)]
+    R=num_list[(length//2):]
+    custom_sort(L)
+    custom_sort(R)
+    combine(L, R, num_list)
+    return num_list
+
+
+def combine(L, R, num_list):
+    lenL=len(L)
+    lenR=len(R)
+    a=0
+    b=0
+    c=0
+    while(a<lenL and b<lenR):
+        if L[a]<=R[b]:
+            num_list[c]=L[a]
+            a+=1
+        else:
+            num_list[c]=R[b]
+            b+=1
+        c+=1
+    while a<lenL:
+        num_list[c]=L[a]
+        a+=1
+        c+=1
+    while b<lenR:
+        num_list[c]=R[b]
+        b+=1
+        c+=1
+
+print(custom_sort([1,7,4,2]))
--- introduction_to_python/recursive_functions/3_towers_of_hanoi/solutions/solution.py
+++ introduction_to_python/recursive_functions/3_towers_of_hanoi/solutions/solution.py
@@ -1,1 +1,23 @@
 # Write your solution here
+"""
+make stack of n-1 disks on aux_rod
+first disk moves to to_rod with even n, first disk moves to aux_rod on odd n
+base case: n=1: return 1
+""" 
+
+#this one was done the way the problem told it to be done
+
+def tower_of_hanoi(n, from_rod="from", to_rod="to", aux_rod="aux"):
+    if n==1:
+        return 1
+    return tower_of_hanoi(n-1, from_rod, aux_rod,to_rod)+1+tower_of_hanoi(n-1, aux_rod, to_rod, from_rod)
+    
+print(tower_of_hanoi(4, "source", "to", "aux"))
+
+
+# #modified so pytest works
+# def tower_of_hanoi(n, from_rod, to_rod, aux_rod):
+#     if n==1:
+#         return 1
+#     return tower_of_hanoi(n-1)+1+tower_of_hanoi(n-1)
+    
