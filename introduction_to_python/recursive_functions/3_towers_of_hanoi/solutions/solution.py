5aa11b0dd146225ac71c54583e8147bdb548abc0
Tara Chugh <Tara@Taras-MacBook.local>
2020-06-30 22:47:44 -0400

    work on exercises

--- introduction_to_python/recursive_functions/3_towers_of_hanoi/solutions/solution.py
+++ introduction_to_python/recursive_functions/3_towers_of_hanoi/solutions/solution.py
@@ -11,7 +11,9 @@ def tower_of_hanoi(n, from_rod="from", to_rod="to", aux_rod="aux"):
     if n==1:
         return 1
     return tower_of_hanoi(n-1, from_rod, aux_rod,to_rod)+1+tower_of_hanoi(n-1, aux_rod, to_rod, from_rod)
-    
+    #making stack of n-1 disks onto the aux rod from the from_rod
+    #the plus 1 in the middle is moving the nth disk to the to_rod from the from_rod 
+    #then moving the stack of n-1 disks from the aux_rod to the to_rod
 print(tower_of_hanoi(4, "source", "to", "aux"))
 
 
